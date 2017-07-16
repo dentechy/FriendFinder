c4e0abcdf5ccdb1dff6c4ffc98f36663e116090f
Dennis Feliciano <dennis.feliciano5@gmail.com>
2017-07-16 18:03:18 -0400

    adds routing

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,12 @@
+
+const friends = require('../data/friends');
+
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  app.post("/api/tables", function(req, res) {
+    friends.push(req.body);
+  })
+};
--- server.js
+++ server.js
@@ -3,8 +3,32 @@ const express = require('express');
 const body-parser = require('body-parser');
 const path = require('path');
 
+
 //Express setup
 const app = express();
 const port = 3500;
 
 
+function homePage(url, req, res) {
+  fs.readFile()
+}
+
+function surveyPage(url, req, res) {
+  fs.readFile()
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//routes defined here
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(port, function(err) {
+  if (err) {
+    return console.error(err)
+  }
+  console.log("App listening on PORT " + port);
+});
