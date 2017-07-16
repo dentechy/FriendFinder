f1570bd8d41903fdcd6ed3683a099269cc9e477b
Dennis Feliciano <dennis.feliciano5@gmail.com>
2017-07-16 18:12:17 -0400

    adds more routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,13 @@
 
+const path = require('path');
 
-app.use(express.static(__dirname + "home"));
+module.exports = function(app) {
+
+  app.get('/survey', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/survey.html'));
+  });
+
+  app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+} 
