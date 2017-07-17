769dacac30f49738a3aa17911be0be11987bedf0
Dennis Feliciano <dennis.feliciano5@gmail.com>
2017-07-17 14:43:12 -0400

    setups survey framework and fixes broken route

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //Defines dependencies
 const express = require('express');
-const body-parser = require('body-parser');
+const bodyParser = require('body-parser');
 const path = require('path');
 
 
